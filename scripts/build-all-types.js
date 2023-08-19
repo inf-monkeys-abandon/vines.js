753c3d8873dcc81e27e7b2cc57d701e5f9a5f3a8
Meeken <meeken1998@gmail.com>
2023-08-20 02:06:21 +0800

    fix

--- scripts/build-all-types.js
+++ scripts/build-all-types.js
@@ -1,38 +1,30 @@
 
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 const prettier = require('prettier/standalone');
 const plugin = require('prettier/parser-typescript');
 
 const modify = (code) => {
   code = code.replace(new RegExp(' declare', 'g'), '');
-  code = code.split('\n').filter(s => !s.includes('import ')).join('\n');
-  return code;
-}
+  return code.split('\n').filter((s) => !s.includes('import ')).join('\n');
+};
+
+const readFileSyncAndModify = (filePath) => {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  return modify(fileContent);
+};
 
 const generateModelTypes = async () => {
   const modelsPath = path.resolve(__dirname, '../lib/models');
-  const files = fs.readdirSync(modelsPath);
-  let contents = [];
-  for (const file of files) {
-    if (!file.endsWith('.d.ts') || file.startsWith('index')) continue;
-    const filePath = path.resolve(modelsPath, file);
-    const fileContent = fs.readFileSync(filePath, "utf8");
-    contents.push(fileContent);
-  }
+  const files = fs.readdirSync(modelsPath).filter((file) => file.endsWith('.d.ts') && !file.startsWith('index'));
+  const contents = files.map((file) => readFileSyncAndModify(path.resolve(modelsPath, file)));
   const clientOptionsPath = path.resolve(__dirname, '../lib/VinesClientOptions.d.ts');
-  const clientOptionsContent = fs.readFileSync(clientOptionsPath, "utf8");
-  contents.push(clientOptionsContent);
-
   const clientPath = path.resolve(__dirname, '../lib/VinesClient.d.ts');
-  const clientContent = fs.readFileSync(clientPath, "utf8");
-  contents.push(clientContent);
-  contents = contents.map(modify);
-
+  contents.push('export type RequestConfig = Record<string, any>;');
+  contents.push(readFileSyncAndModify(clientOptionsPath), readFileSyncAndModify(clientPath));
   const typesCode = `declare module Vines {
     ${contents.join('\n')}
   }`;
-
   const formatted = prettier.format(typesCode, {
     parser: 'typescript',
     plugins: [plugin],
@@ -41,10 +33,8 @@ const generateModelTypes = async () => {
     printWidth: 200,
     semi: true,
   });
-
   const typesFilePath = path.resolve(__dirname, '../lib/all-types.d.ts');
-  fs.unwatchFile(typesFilePath);
   fs.writeFileSync(typesFilePath, formatted);
-}
+};
 
 generateModelTypes().catch(console.error);
