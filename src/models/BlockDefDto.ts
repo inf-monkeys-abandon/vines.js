7e6747219f4e2fe6d66462cce92889d38dc0d535
RealTong <i@wst.sh>
2023-10-26 12:24:06 +0800

    fix(fix): fix
    增加最后检测时间

--- src/models/BlockDefDto.ts
+++ src/models/BlockDefDto.ts
@@ -200,6 +200,7 @@ export interface BlockDefinition {
   rules?: BlockRuleItem[];
   extra?: BlockExtraInfo;
   credentials?: BlockCredentialItem[];
+  lastHealthCheckTime?: number;
   blockStatus: 'available' | 'unavailable'| 'unknown'| 'info';
   totalCheckCount?: number;
   availableCount?: number;
